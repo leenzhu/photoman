867e76d8bfe45e4ac674916793e471feb7298a5c
leenzhu <i@leenzhu.com>
2019-05-28 22:02:10 +0800

    now sync ok

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ type optContex struct {
 	outputDir string
 	inputDir string
 	md5File string
+	dupDir string
 	pathMd5 map[string]string
 	md5Path map[string]string
 }
@@ -27,20 +28,21 @@ func main() {
 	var ctx optContex
 	flag.StringVar(&ctx.outputDir, "o", "", "output directory")
 	flag.StringVar(&ctx.inputDir, "i", "", "input directory")
+	flag.StringVar(&ctx.dupDir, "d", "", "input directory")
 	flag.StringVar(&ctx.md5File, "m", "", "md5 file of output directory")
 	flag.StringVar(&ctx.mode, "M", "check", "work mode: check|sync")
 	flag.Parse()
 
-	if !validOption(ctx) {
+	if !validOption(&ctx) {
 		return
 	}
 
 	switch ctx.mode {
 	case "check":
-		fileCheck(ctx)
+		fileCheck(&ctx)
 	case "sync":
-		fileCheck(ctx)
-		fileSync(ctx)
+		fileCheck(&ctx)
+		fileSync(&ctx)
 	default:
 	}
 
@@ -89,27 +91,41 @@ func getExif(path string) (string, error) {
 }
 
 func getOutput(path, dst string) string {
-	var dstFile string
+	fullName := filepath.Base(path)
 	dt, err := getExif(path)
 	if err != nil {
 		return ""
 	}
 
+	return getUniqueName(filepath.Join(dst, dt, fullName)) 
+}
+
+func getUniqueName(path string) string {
+	var destFile string
 	fullName := filepath.Base(path)
+	dir := filepath.Dir(path)
 	ext := filepath.Ext(fullName)
 	name := strings.TrimSuffix(fullName, ext)
 
 	for idx := 1;;idx++ {
-		dstFile = filepath.Join(dst, dt, fullName)
-		if !exists(dstFile) {
+		destFile = filepath.Join(dir, fullName)
+		if !exists(destFile) {
 			break
 		}
 
 		fullName = fmt.Sprintf("%s_%d%s", name, idx, ext)
-		xlog.Debugf("warning: %s conflict, new name:%s\n", dst, fullName)
+		xlog.Debugf("warning: %s conflict, new name:%s\n", destFile, fullName)
 	}
 
-	return dstFile
+	return destFile
+}
+
+func moveToDup(path, dupDir string) error {
+	fullName := filepath.Base(path)
+	
+	destFile := getUniqueName(filepath.Join(dupDir, fullName))
+	xlog.Infof("MOVDUP %s -> %s\n", path, destFile)
+	return os.Rename(path, destFile)
 }
 
 func moveToDate(path, dest string) error{
@@ -123,10 +139,12 @@ func moveToDate(path, dest string) error{
 		xlog.Errorf("move file failed:%v\n", err)
 		return err
 	}
+	xlog.Infof("SYNC %s -> %s\n", path, dst)
 	return nil
 }
 
-func fileSync(ctx optContex) {
+func fileSync(ctx *optContex) {
+	fileIdx := 0
 	filepath.Walk(ctx.inputDir, func(path string, f os.FileInfo, err error) error {
 		if err != nil {
 			xlog.Errorf("%s error with %v\n", err)
@@ -135,25 +153,28 @@ func fileSync(ctx optContex) {
 		if f.IsDir() {
 			return nil
 		}
-
-		xlog.Debugf("SYNC %s\n", path);
+		fileIdx += 1
+		xlog.Debugf("%06d SYNC %s\n", fileIdx, path);
 
 		md5sum := md5Sum(path)
 
 		if oldPath, ok := ctx.md5Path[md5sum]; ok {
 			xlog.Infof("DUP %s|%s\n", oldPath, path)
+			moveToDup(path, ctx.dupDir)
 		} else {
+			xlog.Debugf("md5 count:%d, md5=%s\n", len(ctx.md5Path), md5sum)
+			os.Exit(0)
 			if err := moveToDate(path, ctx.outputDir); err != nil {
 				xlog.Errorf("Sync %s failed:%v\n", path, err)
 			} else {
-				xlog.Errorf("SYNC %s OK\n", path)
+				xlog.Infof("SYNC %s OK\n", path)
 			}
 		}
 		return nil
 	})
 }
 
-func fileCheck(ctx optContex) {
+func fileCheck(ctx *optContex) {
 	// step 1. load md5.sum records
 	xlog.Debugf("STEP 1 load md5.sum\n")
 	ctx.pathMd5 = loadPathMd5(ctx.md5File)
@@ -168,9 +189,9 @@ func fileCheck(ctx optContex) {
 	}
 
 	// step 3. build md5Path for unique file check
-	xlog.Debugf("STEP 3 build revers md5 Map\n")
+	xlog.Debugf("STEP 3 build revers md5 Map ---\n")
 	ctx.md5Path = reversMap(ctx.pathMd5)
-
+	xlog.Debugf("len(ctx.md5Path)=%d\n", len(ctx.md5Path));
 	// step 4. open temp md5sum file
 	xlog.Debugf("STEP 4 open md5 file for writing\n")
 	file, w, err := openMd5File(ctx.md5File)
@@ -180,6 +201,7 @@ func fileCheck(ctx optContex) {
 
 	// step 5. traval directory process each file
 	xlog.Debugf("STEP 5 process each file\n")
+	fileIdx := 0
 	filepath.Walk(ctx.outputDir, func(path string, f os.FileInfo, err error) error {
 		if err != nil {
 			xlog.Errorf("%s error with %v\n", err)
@@ -188,8 +210,8 @@ func fileCheck(ctx optContex) {
 		if f.IsDir() {
 			return nil
 		}
-
-		xlog.Debugf("Processing %s\n", path)
+		fileIdx += 1
+		xlog.Debugf("%06d Processing %s\n", fileIdx, path)
 
 		if oldMd5sum, ok := ctx.pathMd5[path]; !ok { // this is a new file(path)
 			md5sum := md5Sum(path)
@@ -198,6 +220,7 @@ func fileCheck(ctx optContex) {
 			// if this file has the same md5 with another, ignore this file
 			if oldPath, ok := ctx.md5Path[md5sum]; ok {
 				xlog.Infof("DUP %s|%s\n", oldPath, path)
+				moveToDup(path, ctx.dupDir)
 			} else {
 				xlog.Infof("NEW %s\n", path)
 				ctx.md5Path[md5sum] = path
@@ -294,7 +317,7 @@ func loadMd5File(path string) map[string]string {
 	return pathMd5
 }
 
-func validOption(ctx optContex) bool {
+func validOption(ctx *optContex) bool {
 	if !hasFlag(ctx.outputDir) {
 		xlog.Errorf("ouput option must be provided\n")
 		return false
@@ -310,6 +333,10 @@ func validOption(ctx optContex) bool {
 		return false
 	}
 
+	if !hasFlag(ctx.dupDir) {
+		xlog.Errorf("md5 option must be provided\n")
+		return false
+	}
 	return true
 }
 
